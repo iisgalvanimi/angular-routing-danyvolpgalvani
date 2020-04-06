37c00a117ef8ad05db0fef3b2071da553a111173
Daniele Volpato <volpato.daniele@iisgalvanimi.edu.it>
2020-04-06 11:45:49 +0000

    fatto

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { SearchComponent } from './search/search.component';
+
+const routes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: 'search', component: SearchComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
